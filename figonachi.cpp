76ff1631a69b05e5f80596a63c1cc0cfd6dff6ff
ErickS2023 <141955911+ErickS2023@users.noreply.github.com>
2023-10-24 12:31:15 -0600

    Add files via upload

---
+++ figonachi.cpp
@@ -0,0 +1,102 @@
+#include<stdio.h>
+int leedato(){
+	int n;
+	scanf("%d", &n);
+	return n;
+}
+void fibonacci(){
+	int n, i=0, a=1, b=1, c=0;
+	do{
+		printf("Ingrese el numero de la serie: ");
+		n= leedato();
+	}while(n<2);
+	printf("%d\n%d\n",a,b);
+	
+	while(i<n-2){
+		c=a + b;
+		if(c>=0){
+			printf("%d\n", c);
+		}
+			a=b;
+            b=c;
+            i++;
+	}
+}
+int main(){
+	fibonacci();
+	getchar();
+	
+return 0;	
+}
+	
+
+
+
+
+
+/*
+#include<iostream>
+using namespace std;
+ 
+int leedato() {
+    int n;
+    cin >> n;
+    return n;
+}
+ 
+void fibonacci() {
+    int n, i = 0, a = 1, b = 1, c = 0;
+    do {
+        cout << "Ingrese el numero de la cadena: ";
+        n = leedato();
+    } while (n < 2);
+ 
+    cout << a << endl << b << endl;
+     
+    while (i < n - 2) {
+        c = a + b;
+        cout << c << endl;
+        a = b;
+        b = c;
+        i++;
+    }
+}
+ 
+int main() {
+    fibonacci();
+    cin.ignore();
+    return 0;
+}
+|*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
