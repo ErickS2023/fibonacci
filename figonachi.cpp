2ee1058d0cf0f98069bfdce3495e342977c1e340
ErickS2023 <141955911+ErickS2023@users.noreply.github.com>
2023-10-24 12:32:31 -0600

    Update figonachi.cpp

--- figonachi.cpp
+++ figonachi.cpp
@@ -34,43 +34,6 @@ return 0;
 
 
 
-/*
-#include<iostream>
-using namespace std;
- 
-int leedato() {
-    int n;
-    cin >> n;
-    return n;
-}
- 
-void fibonacci() {
-    int n, i = 0, a = 1, b = 1, c = 0;
-    do {
-        cout << "Ingrese el numero de la cadena: ";
-        n = leedato();
-    } while (n < 2);
- 
-    cout << a << endl << b << endl;
-     
-    while (i < n - 2) {
-        c = a + b;
-        cout << c << endl;
-        a = b;
-        b = c;
-        i++;
-    }
-}
- 
-int main() {
-    fibonacci();
-    cin.ignore();
-    return 0;
-}
-|*/
-
-
-
 
 
 
